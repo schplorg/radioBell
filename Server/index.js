cca103c3d5656793040081b626a6c9c7f0be9ea1
Sam <sam@schplorg.de>
2019-05-15 21:52:01 +0200

    fixed ios safari audio

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
-
 window.onload = function () {
+    var audio = new Audio('dmrdrn2.mp3');
+    initAudio(audio);
     if (window["WebSocket"]) {
         const wsAddr = "ws://" + document.location.host + "/ws";
         console.log("opening " + wsAddr);
@@ -9,13 +10,30 @@ window.onload = function () {
             console.log("Connection closed.");
         };
         conn.onmessage = function (evt) {
+            console.log(evt.data);
             if(evt.data != "tick"){
-                console.log(evt.data);
-                var audio = new Audio('dmrdrn2.mp3');
                 audio.play();
             }
         };
+        conn.onerror = function (evt) {
+            console.log("error!");
+            console.log(evt);
+        }
+        console.log(conn);
     } else {
         console.log("Your browser does not support WebSockets.");
     }
 };
+function initAudio(audio) {
+    audio.addEventListener('play', function () {
+        // When the audio is ready to play, immediately pause.
+        audio.pause();
+        audio.removeEventListener('play', arguments.callee, false);
+    }, false);
+    document.addEventListener('click', function () {
+        // Start playing audio when the user clicks anywhere on the page,
+        // to force Mobile Safari to load the audio.
+        document.removeEventListener('click', arguments.callee, false);
+        audio.play();
+    }, false);
+}
