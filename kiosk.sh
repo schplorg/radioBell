56a876a9b5dc6e060d575844eaf0bb6fc9a295c6
sam <sam@schplorg.de>
2019-10-18 00:08:50 +0200

    added autostart scripts

---
+++ kiosk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/pi
+rm allout.log
+/home/pi/app_arm > allout.log 2>&1 &
+xset s noblank
+xset s off
+xset -dpms
+#unclutter -idle 0.5 -root &
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+pressEnter()
+{
+sleep 10
+#xdotool key Return
+xdotool mousemove 500 500 click 1
+}
+pressEnter &
+chromium-browser --start-fullscreen --noerrdialogs --disable-infobars http://localhost:58000
---
+++ press.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pressEnter()
+{
+sleep 10
+xdotool key Return
+}
+pressEnter &
